03b51288c7a02fe9f82e70f505094e0bda6639ba
TinaYanggao <163075432+TinaYanggao@users.noreply.github.com>
2025-08-20 20:19:12 +0800

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,105 @@
+<!--Hello World!
+
+<h1>Hello World!</h1>
+
+<style>
+    h1{
+        color:blue;
+    }
+</style>
+
+<script>
+    alert("Hello World!");
+    </script>
+-->
+<?php 
+//echo "Hello World!";
+
+?>
+
+<!--Hello -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <!--  <h1>This is my first PHP pages</h1> -->
+
+    <?php //echo //"<b>Hello my name is John!</b><br>"; 
+
+   // aZ09
+  //  $a = 5;
+  //  echo $a;S
+   // var_dump($a);
+    
+  //  var_dump("Tina");
+  //  var_dump(3.14);
+   // var_dump(True);
+   // var_dump([1,2,3]);
+   // var_dump(null);
+
+
+   //global
+  // $a = 5; 
+
+  // echo $a."<br>";
+
+  // function test(){
+  //  $a = 0;
+//    static $a = 0; //static
+   // echo $a."<br>";
+  //  $a++;
+  // }
+
+ //  test();
+  // test();
+
+ //  echo("Hello<br>"), ("Hello<br>"), ("Hello<br>") ;
+  // echo "World!<br>", "World!<br>", "World!<br>" ;
+  // print("Hello<br>");
+   //print "World!<br>";
+//   echo  print("Hello<br>");
+ 
+ // $a = 5;
+ // echo "$a Eggs"
+  // echo 'Hello';
+
+  $a = "John";
+  $b = -5;
+  $c = 3.14;
+  $d = false;
+  $e = [1,2,3];
+  $f = new stdClass();
+  $g = null;
+  $h = fopen("php_practice.php", "r");
+
+  var_dump($a);
+  echo '<br>';
+    var_dump($b);
+  echo '<br>';
+    var_dump($c);
+  echo '<br>';
+    var_dump($d);
+  echo '<br>';
+    var_dump($e);
+  echo '<br>';
+    var_dump($f);
+  echo '<br>';
+    var_dump($g);
+  echo '<br>';
+    var_dump($h);
+  echo '<br>';
+
+
+
+
+    ?>
+
+
+</body>
+</html>
+
